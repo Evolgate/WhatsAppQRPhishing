611ed5de63abdc67c32dfcb86a80c9b91edd4868
Evolgate <81663010+Evolgate@users.noreply.github.com>
2022-09-08 13:20:39 +0200

    Update whatsgrab.py

--- whatsgrab.py
+++ whatsgrab.py
@@ -5,12 +5,13 @@ from PIL import Image
 import imagehash
 import pyautogui
 import sys
+import shutil
 from time import sleep
 
 while True:
-    sleep(2)
     #reactive qrcode
     pyautogui.click(1245, 360)
+    sleep(1)
 
     # part of the screen
     im=ImageGrab.grab(bbox=(1136,225,1400,488))
@@ -20,10 +21,20 @@ while True:
     qrcode = True
     hash0 = imagehash.average_hash(Image.open('img.png')) 
     hash1 = imagehash.average_hash(Image.open('qrcodeexample.png')) 
-    cutoff = 32  # maximum bits that could be different between the hashes. 
+    cutoff = 33  # maximum bits that could be different between the hashes. 
 
-    if hash0 - hash1 > cutoff:
-        qrcode = False
+    if not hash0 - hash1 < cutoff:
+        hash0 = imagehash.average_hash(Image.open('img.png')) 
+        hash1 = imagehash.average_hash(Image.open('qrreload.png')) 
+        cutoff = 33  # maximum bits that could be different between the hashes.
+        shutil.copyfile("qrcodeexample.png", "img.png")
+        if not hash0 - hash1 < cutoff:
+            hash0 = imagehash.average_hash(Image.open('img.png')) 
+            hash1 = imagehash.average_hash(Image.open('white.png')) 
+            cutoff = 33  # maximum bits that could be different between the hashes.
+            shutil.copyfile("qrcodeexample.png", "img.png")
+            if not hash0 - hash1 < cutoff:
+                qrcode = False
 
     
     # to file 
@@ -34,11 +45,10 @@ while True:
         session.storbinary('STOR img.png', file)
         file.close()
         session.quit()
-    else: 
+    if qrcode == False:
         file = open("imgnotavaliable.png",'rb')
         session.cwd("WhatsHook")
         session.storbinary('STOR img.png', file)
         file.close()
         session.quit()
         sys.exit()
-
